3c82aa95039984c870838811256ab1871b1d5e76
Marcell Puskas <puskasmarcell05@gmail.com>
2021-11-13 23:41:08 +0100

    tetris is working
    Took a full day, had to scrap most off the code, but its working now.
    Also updated README.md

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # Matrix Boy
 This is an arduino project, made for a Game Boy like arduino with a 8 x 16 RGB matrix screen and an analog joystick
+## info
+Copyed some of the code from my other project: game-box
 ## Games
  - Snake
- - Tetris (working on)
+ - Tetris
  - Atari breakout (planned)
+ - Atari Space Invaders (planned)
+ - Catch the egg (planned)
+## Todo
+ - have a menu and separate files for games
 ## Pictures
 <img src="https://raw.githubusercontent.com/Marcell-Puskas/matrix-boy/main/images/game_snake.jpg">
 <img src="https://raw.githubusercontent.com/Marcell-Puskas/matrix-boy/main/images/menu_snake.jpg">
--- tetris/matrix_boy_tetris.ino
+++ tetris/matrix_boy_tetris.ino
@@ -2,6 +2,10 @@
 #include <Adafruit_NeoMatrix.h>
 #include <Adafruit_NeoPixel.h>
 
+#define JOYX A0
+#define JOYY A1
+#define JOYB 9
+
 int joy_x = 0;
 int joy_y = 0;
 int joy_button = 0;
@@ -18,99 +22,353 @@ Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(
     matrixWidth, matrixHeight, tilesX, tilesY, pin, matrixType, ledType);
 
 void setup() {
-    pinMode(6, OUTPUT);
     matrix.begin();
     matrix.setBrightness(1);
     Serial.begin(9600);
 }
 
+//config
+const int mapx = 8;
+const int mapy = 16;
+const int tetro_num = 7;
+const int mino_num = 4;
+const int input_update = 10;
+const int clear_time = 400;
+const int score_time = 2000;
+const int default_timeout = 10;
+const int down_timeout = 3;
+const int up_timeout = 14;
+const int restart_timeout = 50;
+
+const int tetro_cordinates[][4][2] = {
+    {{0, 1}, {1, 1}, {2, 1}, {3, 1}},
+    {{0, 0}, {0, 1}, {1, 1}, {2, 1}},
+    {{2, 0}, {0, 1}, {1, 1}, {2, 1}},
+    {{0, 0}, {1, 0}, {0, 1}, {1, 1}},
+    {{1, 0}, {2, 0}, {0, 1}, {1, 1}},
+    {{1, 0}, {0, 1}, {1, 1}, {2, 1}},
+    {{0, 0}, {1, 0}, {1, 1}, {2, 1}}
+};
+
+const int tetro_offsets[][2] = {
+    {3, 3},
+    {2, 2},
+    {2, 2},
+    {1, 1},
+    {2, 2},
+    {2, 2},
+    {2, 2}
+};
+
+const uint32_t tetro_colors[] = {
+    matrix.Color(0, 255, 255),
+    matrix.Color(0, 0, 255),
+    matrix.Color(255, 170, 0),
+    matrix.Color(255, 255, 0),
+    matrix.Color(0, 255, 0),
+    matrix.Color(255, 0, 255),
+    matrix.Color(255, 0, 0)
+};
+
+const uint32_t background_color = matrix.Color(0, 0, 0);
+const uint32_t full_line_color = matrix.Color(255, 255, 255);
+const uint32_t pause_color = matrix.Color(255, 255, 255);
+
+bool run, gameover;
+
+int posx, posy, dir, selected_index, points, speed, timeout_x, timeout_y, timeout_b;
+
+char keychar, movechar;
+
+int selected_tetro[mino_num][2];
+int construncted_tetro[mino_num][2];
+int stack_map[mapx][mapy];
+
 void loop()
 {
+    Tetris_game();
+}
+
+void Construct_tetro(int cdir, bool select = false)
+{
+    for (int cmino = 0; cmino < mino_num; cmino++)
+    {
+        switch (cdir)
+        {
+            case 0:
+            construncted_tetro[cmino][0] = tetro_cordinates[selected_index][cmino][0];
+            construncted_tetro[cmino][1] = tetro_cordinates[selected_index][cmino][1];
+            break;
+
+            case 1:
+            construncted_tetro[cmino][0] = tetro_offsets[selected_index][0] - tetro_cordinates[selected_index][cmino][1];
+            construncted_tetro[cmino][1] = tetro_cordinates[selected_index][cmino][0];
+            break;
+
+            case 2:
+            construncted_tetro[cmino][0] = tetro_offsets[selected_index][0] - tetro_cordinates[selected_index][cmino][0];
+            construncted_tetro[cmino][1] = tetro_offsets[selected_index][1] - tetro_cordinates[selected_index][cmino][1];
+            break;
+
+            case 3:
+            construncted_tetro[cmino][0] = tetro_cordinates[selected_index][cmino][1];
+            construncted_tetro[cmino][1] = tetro_offsets[selected_index][1] - tetro_cordinates[selected_index][cmino][0];
+            break;
+        }
+    }
+    if(select)
+    {
+        for (int cmino = 0; cmino < mino_num; cmino++)
+        {
+            selected_tetro[cmino][0] = construncted_tetro[cmino][0];
+            selected_tetro[cmino][1] = construncted_tetro[cmino][1];
+        }
+    }
+}
+
+void New_tetro()
+{
+    dir = 0;
+    selected_index = random(tetro_num);
+    Construct_tetro(dir, true);
+
+    posx = mapx / 2 - tetro_offsets[selected_index][0] / 2;
+    if(tetro_offsets[selected_index][1] == 3) posy = -1;
+    else posy = 0;
+}
+
+void Screen_print()
+{
+    //draw stack
+    for (int cy = 0; cy < mapy; cy++)
+    {
+        for (int cx = 0; cx < mapx; cx++)
+        {
+            if(stack_map[cx][cy] != 0)
+                matrix.drawPixel(cx, cy, (tetro_colors[stack_map[cx][cy] - 1]) );
+            else
+                matrix.drawPixel(cx, cy, background_color);
+        }
+    }
+
+    //draw current block
+    for (size_t cmino = 0; cmino < mino_num; cmino++)
+    {
+        matrix.drawPixel(
+            posx + selected_tetro[cmino][0],
+            posy + selected_tetro[cmino][1],
+            tetro_colors[selected_index]);
+    }
+
+    matrix.show();
+}
+
+void Key_get()
+{
+    for (size_t t = 0; t < speed && run; t++)
+    {
+        if(Serial.available())
+        {
+            keychar = Serial.read();
+            switch(keychar)
+            {
+                case 'a': movechar = 'l'; break;
+                case 'd': movechar = 'r'; break;
+                case 'w': movechar = 'u'; break;
+                case 's': movechar = 'd'; break;
+                case 'm': run = false; break;
+            }
+        }
+
+        if(analogRead(JOYX) < 384)
+        {
+            if(timeout_x == 0)
+            {
+                movechar = 'l';
+                timeout_x =  default_timeout;
+            }
+        }
+        else if(analogRead(JOYX) > 640)
+        {
+            if(timeout_x == 0)
+            {
+                movechar = 'r';
+                timeout_x =  default_timeout;
+            }
+        }
+        else timeout_x = 0;
+
+        if(analogRead(JOYY) < 384)
+        {
+            if(timeout_y == 0)
+            {
+                movechar = 'u';
+                timeout_y =  up_timeout;
+            }
+        }
+        else if(analogRead(JOYY) > 640)
+        {
+            if(timeout_y == 0)
+            {
+                movechar = 'd';
+                timeout_y = down_timeout;
+            }
+        }
+        else timeout_y = 0;
+
+        if(digitalRead(JOYB) == 0)
+        {
+            if(timeout_b == 0)
+            {
+                for (size_t i = 0; i < 200 && digitalRead(JOYB) == 0; i++)
+                    delay(input_update);
+                
+                if(digitalRead(JOYB) == 0)
+                {
+                    run = false;
+                    timeout_b = restart_timeout;
+                }
+                else
+                {
+                    matrix.drawRect(1, 4, 2, 8, pause_color);
+                    matrix.drawRect(5, 4, 2, 8, pause_color);
+                    matrix.show();
+                    while (digitalRead(JOYB) == 1)
+                        delay(input_update);
+                    
+                    timeout_b = default_timeout;
+                }
+            }
+        }
+        else timeout_b = 0;
+
+        switch(movechar)
+        {
+            case 'l': if(check_move(posx - 1, posy, dir)) posx--; break;
+            case 'r': if(check_move(posx + 1, posy, dir)) posx++; break;
+            case 'd': if(check_move(posx, posy + 1, dir)) posy++; break;
+
+            case 'u':
+            if(check_move(posx, posy, (dir + 1) % 4))
+            {
+                dir = (dir + 1) % 4;
+                Construct_tetro(dir, true);
+            }
+            break;
+        }
+
+        if(timeout_x != 0) timeout_x--;
+        if(timeout_y != 0) timeout_y--;
+        if(timeout_b != 0) timeout_b--;
+        
+        movechar = NULL;
+
+        Screen_print();
+
+        delay(input_update);
+    }
+}
+
+bool check_move(int nextX, int nextY, int nextDir)
+{
+    Construct_tetro(nextDir);
+    for(int cmino = 0; cmino < mino_num; cmino++)
+    {
+        if(nextX + construncted_tetro[cmino][0] >= mapx) return false;
+        if(nextX + construncted_tetro[cmino][0] < 0) return false;
+        if(nextY + construncted_tetro[cmino][1] >= mapy) return false;
+        if(nextY + construncted_tetro[cmino][1] < 0) return false;
+
+        if(stack_map[ nextX + construncted_tetro[cmino][0] ][ nextY + construncted_tetro[cmino][1] ] != 0)
+            return false;
+    }
+    return true;
+}
+
+void Check_full_line()
+{
+    bool lines_to_clear = false;
+    for (size_t cline = 0; cline < mapy; cline++)
+    {
+        bool line_full = true;
+        for (size_t crow = 0; crow < mapx; crow++)
+            if(stack_map[crow][cline] == 0)
+                line_full = false;
+        
+        if(line_full)
+        {
+            matrix.drawLine(0, cline, mapx - 1, cline, full_line_color);
+            
+            lines_to_clear = true;
+            points++;
+
+            for (size_t copyy = cline; copyy >= 1; copyy--)
+                    for (size_t copyx = 0; copyx < mapx; copyx++)
+                        stack_map[copyx][copyy] = stack_map[copyx][copyy - 1];
+        }
+    }
+
+    if(lines_to_clear)
+    {
+        matrix.show();
+        delay(clear_time);
+    }
+}
+
+void Check_gameover()
+{
+    if (!check_move(posx, posy, dir))
+    {
+        gameover = true;
+        run = false;
+    }
+}
+
+void Logic()
+{
+    if(check_move(posx, posy + 1, dir))
+    {
+        posy += 1;
+    }
+    else
+    {
+        for(int cmino = 0; cmino < mino_num; cmino++)
+        {
+            stack_map[ posx + selected_tetro[cmino][0] ][ posy + selected_tetro[cmino][1] ] = selected_index + 1;
+        }
+        Check_full_line();
+        New_tetro();
+        Check_gameover();
+    }
+
+    Screen_print();
+}
+
+void Tetris_game()
+{
+    Serial.println("Starting tetris");
 	matrix.clear();
 
-    //config
-    int mapx = 8;
-    int mapy = 16;
+    points = 0;
+    speed = 50;
+    run = true;
+    gameover = false;
+    memset(stack_map, 0, sizeof(stack_map));
 
-    /*int blI[][4] { 
-        {0, 0, 0, 0},
-        {1, 1, 1, 1}, 
-        {0, 0, 0, 0},
-        {0, 0, 0, 0}
-    };
-    int blJ[][4] { 
-        {0, 0, 0, 0},
-        {0, 1, 0, 0},
-        {0, 1, 1, 1},
-        {0, 0, 0, 0}
-    };
-    int blL[][4] {
-        {0, 0, 0, 0},
-        {0, 0, 1, 0},
-        {1, 1, 1, 0},
-        {0, 0, 0, 0}
-    };
-    int blO[][4] {
-        {0, 0, 0, 0},
-        {0, 1, 1, 0},
-        {0, 1, 1, 0},
-        {0, 0, 0, 0}
-    };
-    int blS[][4] {
-        {0, 0, 0, 0},
-        {0, 0, 1, 1},
-        {0, 1, 1, 0},
-        {0, 0, 0, 0}
-    };
-    int blT[][4] {
-        {0, 1, 0},
-        {1, 1, 1}
-    };
-    int blZ[][4] {
-        {1, 1, 0},
-        {0, 1, 1}
-    };*/
-    /* typedef struct blck
-    {
-        int x;
-        int y;
-    };
+    New_tetro();
     
-    blck testb[] = {
-        {x = 1, testb->y = 2},
-        {testb->x = 1, testb->y = 3}
-    }; */
-
-    /* int blI[][2] = {{}, {}, {}, {}};
-    int blJ[][2] = {{}, {}, {}, {}};
-    int blL[][2] = {{}, {}, {}, {}};
-    int blO[][2] = {{}, {}, {}, {}};
-    int blS[][2] = {{}, {}, {}, {}};
-    int blT[][2] = {{}, {}, {}, {}};
-    int blZ[][2] = {{}, {}, {}, {}}; */
-
-    int block_cordinates[][4][2] = {
-        {{0, 1}, {1, 1}, {2, 1}, {3, 1}},
-        {{0, 0}, {0, 1}, {1, 1}, {2, 1}},
-        {{2, 0}, {0, 1}, {1, 1}, {2, 1}},
-        {{0, 0}, {1, 0}, {0, 1}, {1, 1}},
-        {{1, 0}, {2, 0}, {0, 1}, {1, 1}},
-        {{1, 0}, {0, 1}, {1, 1}, {2, 1}},
-        {{0, 0}, {1, 0}, {1, 1}, {2, 1}}
-    };
-
-    int block_offsets[][2] = {
-
-    };
-
-    /* int *blocks[] = 
+    while(run)
     {
-        blI,
-        blJ,
-        blL,
-        blO,
-        blS,
-        blT,
-        blZ
-    }; */
+        Key_get();
+        Logic();
+    }
+
+    if(gameover)
+    {
+        matrix.clear();
+        matrix.setCursor(0, 0);
+        matrix.print(points);
+        matrix.show();
+        delay(score_time);
+    }
 }
