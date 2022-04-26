7f5fa6f72ce906b89f6632eee203f95452c5f1b9
Marcell-Puskas <puskasmarcell05@gmail.com>
2022-04-26 12:30:08 +0200

    test commit

--- matrix_boy/Tetris_game.cpp
+++ matrix_boy/Tetris_game.cpp
@@ -124,9 +124,8 @@ void Tetris::Update_input()
                 {
                     gyro_down_timeout = millis();
                     if(check_move(posx, posy + 1, dir)) posy++;
-                    Serial.print("nononononononononononononononononononononononono");
+                    Serial.println(milllis()); //debug
                 }
-                Serial.println(millis());
             }
         }
         
