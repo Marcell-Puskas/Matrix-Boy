2699aacb22a5992c2a1ed036357518e1ec7fc6b6
Marcell Puskas <puskasmarcell05@gmail.com>
2022-06-07 22:00:39 +0200

    🕹️Updated Input()
    Input() function now is independent from the delay in game loops

--- matrix_boy/Input.cpp
+++ matrix_boy/Input.cpp
@@ -2,6 +2,8 @@
 
 extern bool gyro_mode;
 long xtime, ytime, btime;
+long blasttime;
+bool b_pressed = false;
 
 void Input(bool no_timeout)
 {
@@ -9,73 +11,84 @@ void Input(bool no_timeout)
 
     if(Serial.available()) keychar = Serial.read();
 
+    /* Serial.print(analogRead(JOYX));
+    Serial.print("\t");
+    Serial.print(analogRead(JOYY));
+    Serial.print("\t");
+    Serial.println(digitalRead(JOYB)); */
+
     if(analogRead(JOYX) < joy_low_threshold)
     {
-        if(xtime + left_timeout < millis() || no_timeout)
+        if(xtime< millis() || no_timeout)
         {
             keychar = 'a';
-            xtime =  millis();
+            xtime =  millis() + left_timeout ;
         }
     }
     else if(analogRead(JOYX) > joy_high_threshold)
     {
-        if(xtime + right_timeout < millis() || no_timeout)
+        if(xtime < millis() || no_timeout)
         {
             keychar = 'd';
-            xtime =  millis();
+            xtime =  millis() + right_timeout;
         }
     }
     else xtime = 0;
 
     if(analogRead(JOYY) < joy_low_threshold)
     {
-        if(ytime + up_timeout < millis() || no_timeout)
+        if(ytime < millis() || no_timeout)
         {
             keychar = 'w';
-            ytime =  millis();
+            ytime =  millis() + up_timeout;
         }
     }
     else if(analogRead(JOYY) > joy_high_threshold)
     {
-        if(ytime + down_timeout < millis() || no_timeout)
+        if(ytime < millis() || no_timeout)
         {
             keychar = 's';
-            ytime = millis();
+            ytime = millis() + down_timeout;
         }
     }
     else ytime = 0;
 
-    if(digitalRead(JOYB) == 0)
+    if(digitalRead(JOYB) == b_down)
     {
-        if(btime + button_timeout < millis())
+        if(btime < millis())
         {
-            for (size_t i = 0; i < exit_timeout && digitalRead(JOYB) == 0; i++)
-                delay(input_update);
-            
-            if(digitalRead(JOYB) == 0)
+            if(millis() - blasttime > 1000)
             {
                 keychar = 'e';
-                timeout_b = restart_timeout;
+                btime = millis() + exit_timeout;
+                b_pressed = false;
             }
             else
             {
-                if(pause_menu)
-                {
-                    matrix.drawRect(1, 4, 2, 8, pause_color);
-                    matrix.drawRect(5, 4, 2, 8, pause_color);
-                    matrix.show();
-                    while (digitalRead(JOYB) == 1)
-                        delay(input_update);
-                    
-                    timeout_b = button_timeout;
-                }
-                else
-                {
-                    keychar = 'o';
-                    timeout_b = button_timeout;
-                }
+                b_pressed = true;
             }
         }
     }
-    else btime = 0;
+    else 
+    {
+        if(b_pressed)
+        {   
+            if(pause_menu)
+            {
+                matrix.drawRect(mapx / 2 - 3, mapy / 2 - 4, 2, 8, pause_color);
+                matrix.drawRect(mapx / 2 + 1, mapy / 2 - 4, 2, 8, pause_color);
+                matrix.show();
+                while (digitalRead(JOYB) == b_up)
+                    delay(input_update);
+                btime = millis() + restart_timeout;
+            }
+            else
+            {
+                keychar = 'o';
+                btime = millis() + button_timeout;
+            }
+            b_pressed = false;
+        }
+        blasttime = millis();
+    }
 }
